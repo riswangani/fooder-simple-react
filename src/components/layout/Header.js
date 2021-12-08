d13b3af8ad370dfcaa130e9a114be4034e237cbf
Riswan Gani Padilah <riswangani11@gmail.com>
2021-12-08 11:29:54 +0700

    first commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>FooderReact</h1>
+      </header>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
