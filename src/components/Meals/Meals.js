fe6e32818ab7534e3c44817749251bc338e1ee07
Riswan Gani Padilah <riswangani11@gmail.com>
2021-12-09 06:20:11 +0700

    create meal item to the screen

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AvaliableMeals from './AvaliableMeals';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaliableMeals />
+    </>
+  );
+};
+
+export default Meals;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import mealsImage from '../../assets/images/meals.jpeg';
+import classes from './Header.module.css';
+import HeaderCardButton from './HeaderCardButton';
+
 const Header = (props) => {
   return (
     <>
-      <header>
+      <header className={classes.header}>
         <h1>FooderReact</h1>
+        <HeaderCardButton />
       </header>
-      <div></div>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='table ful of delicious food!' />
+      </div>
     </>
   );
 };
