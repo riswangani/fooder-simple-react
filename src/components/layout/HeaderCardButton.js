50e128ab910235cbab8d7a321c0a017d5c9b2746
Riswan Gani Padilah <riswangani11@gmail.com>
2021-12-11 07:41:57 +0700

    last commit make a cart function

--- src/components/layout/HeaderCardButton.js
+++ src/components/layout/HeaderCardButton.js
@@ -1,17 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 
 function HeaderCardButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItem = cartCtx.items.reduce((crntNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItem = items.reduce((crntNumber, item) => {
     return crntNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button}  ${
+    btnIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
